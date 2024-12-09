0e4a0561e0eab6c00ca653e0e0361dc18648813c
Oleksandr Protasenko <oleksandr.p@zentixsoft.com>
2024-12-09 21:44:37 +0200

    hotfix: fixed getDate utils function

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,15 @@
-export const rounded = (number, roundedIndex) =>{
-    const result = +number
-    return Number.isInteger(result) ? number : `${result.toFixed(roundedIndex)}`;
-
-}
-export const getDate = ()=>{
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = (`${date.getMonth() + 1}`).length === 1 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
-    const day = date.getDate();
-    return `${year}${month}${day}`
-}
+export const rounded = (number, roundedIndex) => {
+  const result = +number;
+  return Number.isInteger(result) ? number : `${result.toFixed(roundedIndex)}`;
+};
+export const getDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month =
+    `${date.getMonth() + 1}`.length === 1
+      ? `0${date.getMonth() + 1}`
+      : `${date.getMonth() + 1}`;
+  let day = 1;
+  day = day.length > 1 ? day : "0" + day;
+  return `${year}${month}${day}`;
+};
